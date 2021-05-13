63f82013214a064ac64e075a627dc60ecfe203f9
kyriosdata <fabio@inf.ufg.br>
2021-05-13 16:29:53 -0300

    informacoes

--- maio/App.js
+++ maio/App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Button, View } from "react-native";
 
 import ProteseLista from "./components/ProteseLista";
 import Proteses from "./data/Proteses";
+import Informacao from "./components/Informacao";
 
 function onPress(chave) {
   console.log("item clicked", chave);
 }
 
+function informacao() {
+  console.log("abrir modal");
+}
+
 export default function App() {
+  const [showInfo, setShowInfo] = useState(false);
+
+  function fechaInformacao() {
+    setShowInfo(false);
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.boasvindas}>Protótipo Maio</Text>
+      <Button title={"Informação"} onPress={() => setShowInfo(true)} />
+      <Informacao visible={showInfo} onClose={fechaInformacao} />
 
       <ProteseLista lista={Proteses.totais()} onPress={onPress} />
-
       <StatusBar style="auto" />
     </View>
   );
---
+++ maio/components/Informacao.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, Modal, Button, StyleSheet } from "react-native";
+
+const Informacao = ({ visible, onClose }) => {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.linha}>
+        <Text>Prótese Dentária</Text>
+        <Button title={"Fechar"} onPress={onClose} />
+      </View>
+
+      <View style={styles.info}>
+        <View style={styles.bloco}>
+          <View style={styles.rotulo}>
+            <Text style={styles.vertical}>Professores</Text>
+          </View>
+          <View>
+            <Text>Érica Miranda de Torres</Text>
+            <Text>Aderico Santana Guilherme</Text>
+            <Text>Carlos de Paula e Souza</Text>
+            <Text>Célio Umberto de Araújo</Text>
+            <Text>Cláudio Rodrigues Leles</Text>
+            <Text>Fábio Nogueira de Lucena</Text>
+            <Text>Hugo de Carvalho Júnior</Text>
+            <Text>Ricardo Alexandre Zavanelli</Text>
+            <Text>Willian Barnabé</Text>
+          </View>
+        </View>
+
+        <View style={styles.bloco}>
+          <View style={styles.rotulo}>
+            <Text style={styles.vertical}>Estudantes</Text>
+          </View>
+          <View>
+            <Text>Fábio Lisita Moreira Filho</Text>
+            <Text>Fernandes de Carvalho</Text>
+            <Text>Larissa Mendes de Souza Jorge</Text>
+            <Text>Mariana Luísa Fernandes de Carvalho</Text>
+            <Text>Marisa Alves Araújo</Text>
+            <Text>Matheus Pereira Marques</Text>
+            <Text>Thalita Fernanes Fleury Curado</Text>
+            <Text>Thiêssy Tamylla de Freitas</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.linha}>
+        <Text>os três ícones</Text>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  linha: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  info: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    backgroundColor: "yellow",
+  },
+
+  bloco: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  rotulo: {
+    width: 80,
+  },
+
+  vertical: {
+    transform: [{ rotate: "270deg" }],
+  },
+});
+
+export default Informacao;
