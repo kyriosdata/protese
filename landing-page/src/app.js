f7a5c2778bfb549745cc9791784eae98a9a88c6c
kyriosdata <fabio@inf.ufg.br>
2021-06-06 18:33:04 -0300

    landingpage

---
+++ landing-page/src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Start from "./components/Start";
+import "./styles/styles.scss";
+
+ReactDOM.render(<Start />, document.getElementById("app"));
---
+++ landing-page/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: [path.resolve(__dirname, "node_modules")],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+};
