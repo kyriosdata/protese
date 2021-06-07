c2ab13141c227c87f0e9e70685bfffeb482ff21b
kyriosdata <fabio@inf.ufg.br>
2021-06-07 17:56:49 -0300

    estilo

--- landing-page/src/components/Start.js
+++ landing-page/src/components/Start.js
@@ -7,16 +7,27 @@ export default class Start extends React.Component {
       <div>
         <div className="header">Uma ferramenta de ensino!</div>
         <div id="hero">
-          <img src="protese-logo.png" width="100" />
-          <Header
-            title={"Próteses dentárias"}
-            subtitle={"Ambiente-se com os mais variados tipos de próteses"}
-          />
-          <img src="telas.png" />
+          <img src="protese-logo.png" width="80" />
+          <div className="linha">
+            <div className="titulo">
+              <Header
+                title={"Próteses dentárias"}
+                subtitle={"Ambiente-se com os mais variados tipos de próteses"}
+              />
+            </div>
+            <div className="telas">
+              <img src="telas.png" height="400px" />
+            </div>
+          </div>
         </div>
 
-        <img src="expo.png" />
-        <img src="stores.png" />
+        <div className="download">
+          <img src="expo.png" height="30px" className="expo" />
+          <div className="stores">
+            <img src="stores.png" />
+          </div>
+        </div>
+
         <div className="realizacao">
           <img src="logo-fo.png" width="80" />
           <img src="logo-inf.png" width="65" />
